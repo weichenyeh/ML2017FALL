39ea6794499a8784c4aba19557910c6c783d4701
weichenyeh <r06922130@ntu.edu.tw>
2017-09-16 11:49:30 +0800

    hw0 update: no dict() and minor adjustment

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -9,12 +9,9 @@ orig_PixelAccess = orig_im.load()
 new_im = Image.new(orig_im.mode, orig_im.size)
 new_PixelAccess = new_im.load()
 
-f= open('rgb.txt', 'w')
-
 for i in range(new_im.size[0]):
 	for j in range(new_im.size[1]):
 		new_PixelAccess[i, j] = (((orig_PixelAccess[i, j])[0])//2, ((orig_PixelAccess[i, j])[1])//2, ((orig_PixelAccess[i, j])[2])//2)
-		f.write(str(((orig_PixelAccess[i, j])[0])//2) + ',' + str(((orig_PixelAccess[i, j])[1])//2) + ',' + str(((orig_PixelAccess[i, j])[2])//2)+'\n')
 new_im.save('Q2.png')
 
 
