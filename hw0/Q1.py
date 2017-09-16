0536b6cd2802b35d703f011e7ccd76c044f944da
weichenyeh <r06922130@ntu.edu.tw>
2017-09-16 11:43:05 +0800

    hw0 upload

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -5,21 +5,20 @@ f = open(filename, 'r')
 
 aStr = f.read()
 aList = aStr.split()
-aDict = dict()
-
-for word in aList:
-    if word in aDict:
-        aDict[word] += 1
-    else:
-        aDict[word] = 1
-
+bList = []
 
 f = open('Q1.txt', 'w')
 
 cnt = 0
-for key, value in aDict.items():
-    f.write(str(key) + ' ' + str(cnt) + ' ' + str(value) + '\n')
-    cnt += 1
+
+for word in aList:
+	if(word not in bList):
+		bList.append(word)
+		f.write(word + ' ' + str(cnt) + ' ' + str(aList.count(word)) + '\n')
+		cnt += 1
+		
+
+
 
 
 
--- hw0/Q2.py
+++ hw0/Q2.py
@@ -14,7 +14,7 @@ f= open('rgb.txt', 'w')
 for i in range(new_im.size[0]):
 	for j in range(new_im.size[1]):
 		new_PixelAccess[i, j] = (((orig_PixelAccess[i, j])[0])//2, ((orig_PixelAccess[i, j])[1])//2, ((orig_PixelAccess[i, j])[2])//2)
-#		f.write(str(((orig_PixelAccess[i, j])[0])//2) + ',' + str(((orig_PixelAccess[i, j])[1])//2) + ',' + str(((orig_PixelAccess[i, j])[2])//2) )
+		f.write(str(((orig_PixelAccess[i, j])[0])//2) + ',' + str(((orig_PixelAccess[i, j])[1])//2) + ',' + str(((orig_PixelAccess[i, j])[2])//2)+'\n')
 new_im.save('Q2.png')
 
 
