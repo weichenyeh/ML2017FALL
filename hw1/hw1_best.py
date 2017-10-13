1916373b2cf463a0123bdc00aa5037519b88ad26
weichenyeh <r06922130@ntu.edu.tw>
2017-10-13 19:38:12 +0800

    final version 1

--- hw1/hw1_best.py
+++ hw1/hw1_best.py
@@ -6,9 +6,6 @@ import math
 import sys
 import pandas as pd
 
-from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Activation
-
 import sys
 
 inputFile = sys.argv[1]
@@ -31,7 +28,7 @@ def testScale(np_arr, maxi, mini):
 # read model
 [max_x, min_x, max_sqr_x, min_sqr_x] = np.load('hw1_best_scaling_info.npy')
 
-w = load_model('hw1_best_model')
+w = np.load('hw1_best_model.npy')
 
 
 test_x = []
@@ -75,20 +72,17 @@ test_x_scaled = np.concatenate((test_x_scaled,test_sqr_x_scaled), axis=1)
 
 
 # add bias
-# test_x_scaled = np.concatenate((np.ones((test_x_scaled.shape[0],1)),test_x_scaled), axis=1)
+test_x_scaled = np.concatenate((np.ones((test_x_scaled.shape[0],1)),test_x_scaled), axis=1)
 # df_test_x = pd.DataFrame(test_x_scaled)
 # df_test_x.to_csv('df_test_x.csv')
 
 
-pred = w.predict(test_x_scaled)
 
 ans = []
-for i in range(len(pred)):
+for i in range(len(test_x_scaled)):
     ans.append(["id_"+str(i)])
-    a = pred[i].item(0)
+    a = np.dot(w,test_x_scaled[i])
     ans[i].append(a)
-
-# print(ans)
 
 filename = outputFile
 text = open(filename, "w+")
